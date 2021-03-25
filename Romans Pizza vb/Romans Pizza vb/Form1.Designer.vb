139fa55d79ce762832fab9d3dfe8b7951f7850e6
UzairGora <71663883+UzairGora@users.noreply.github.com>
2021-03-25 13:16:03 +0200

    Add files via upload

---
+++ Romans Pizza vb/Romans Pizza vb/Form1.Designer.vb
@@ -0,0 +1,371 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.largeCheckbox = New System.Windows.Forms.CheckBox()
+        Me.mediumCheckbox = New System.Windows.Forms.CheckBox()
+        Me.smallCheckbox = New System.Windows.Forms.CheckBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.tomatoesCheckbox = New System.Windows.Forms.CheckBox()
+        Me.greenPepperCheckbox = New System.Windows.Forms.CheckBox()
+        Me.onionsCheckbox = New System.Windows.Forms.CheckBox()
+        Me.blackOlivesCheckbox = New System.Windows.Forms.CheckBox()
+        Me.mushroomsCheckbox = New System.Windows.Forms.CheckBox()
+        Me.extraCheeseCheckbox = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.totalTextbox = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.largeCheckbox)
+        Me.GroupBox1.Controls.Add(Me.mediumCheckbox)
+        Me.GroupBox1.Controls.Add(Me.smallCheckbox)
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 40)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(142, 159)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Size"
+        '
+        'largeCheckbox
+        '
+        Me.largeCheckbox.AutoSize = True
+        Me.largeCheckbox.Location = New System.Drawing.Point(7, 102)
+        Me.largeCheckbox.Name = "largeCheckbox"
+        Me.largeCheckbox.Size = New System.Drawing.Size(53, 17)
+        Me.largeCheckbox.TabIndex = 2
+        Me.largeCheckbox.Text = "Large"
+        Me.largeCheckbox.UseVisualStyleBackColor = True
+        '
+        'mediumCheckbox
+        '
+        Me.mediumCheckbox.AutoSize = True
+        Me.mediumCheckbox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.mediumCheckbox.Location = New System.Drawing.Point(7, 61)
+        Me.mediumCheckbox.Name = "mediumCheckbox"
+        Me.mediumCheckbox.Size = New System.Drawing.Size(66, 17)
+        Me.mediumCheckbox.TabIndex = 1
+        Me.mediumCheckbox.Text = "Medium "
+        Me.mediumCheckbox.UseVisualStyleBackColor = True
+        '
+        'smallCheckbox
+        '
+        Me.smallCheckbox.AutoSize = True
+        Me.smallCheckbox.Location = New System.Drawing.Point(6, 19)
+        Me.smallCheckbox.Name = "smallCheckbox"
+        Me.smallCheckbox.Size = New System.Drawing.Size(51, 17)
+        Me.smallCheckbox.TabIndex = 0
+        Me.smallCheckbox.Text = "Small"
+        Me.smallCheckbox.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.tomatoesCheckbox)
+        Me.GroupBox2.Controls.Add(Me.greenPepperCheckbox)
+        Me.GroupBox2.Controls.Add(Me.onionsCheckbox)
+        Me.GroupBox2.Controls.Add(Me.blackOlivesCheckbox)
+        Me.GroupBox2.Controls.Add(Me.mushroomsCheckbox)
+        Me.GroupBox2.Controls.Add(Me.extraCheeseCheckbox)
+        Me.GroupBox2.Location = New System.Drawing.Point(195, 40)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(310, 159)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Toppings"
+        '
+        'tomatoesCheckbox
+        '
+        Me.tomatoesCheckbox.AutoSize = True
+        Me.tomatoesCheckbox.Location = New System.Drawing.Point(136, 102)
+        Me.tomatoesCheckbox.Name = "tomatoesCheckbox"
+        Me.tomatoesCheckbox.Size = New System.Drawing.Size(73, 17)
+        Me.tomatoesCheckbox.TabIndex = 5
+        Me.tomatoesCheckbox.Text = "Tomatoes"
+        Me.tomatoesCheckbox.UseVisualStyleBackColor = True
+        '
+        'greenPepperCheckbox
+        '
+        Me.greenPepperCheckbox.AutoSize = True
+        Me.greenPepperCheckbox.Location = New System.Drawing.Point(136, 61)
+        Me.greenPepperCheckbox.Name = "greenPepperCheckbox"
+        Me.greenPepperCheckbox.Size = New System.Drawing.Size(92, 17)
+        Me.greenPepperCheckbox.TabIndex = 4
+        Me.greenPepperCheckbox.Text = "Green Pepper"
+        Me.greenPepperCheckbox.UseVisualStyleBackColor = True
+        '
+        'onionsCheckbox
+        '
+        Me.onionsCheckbox.AutoSize = True
+        Me.onionsCheckbox.Location = New System.Drawing.Point(136, 19)
+        Me.onionsCheckbox.Name = "onionsCheckbox"
+        Me.onionsCheckbox.Size = New System.Drawing.Size(59, 17)
+        Me.onionsCheckbox.TabIndex = 3
+        Me.onionsCheckbox.Text = "Onions"
+        Me.onionsCheckbox.UseVisualStyleBackColor = True
+        '
+        'blackOlivesCheckbox
+        '
+        Me.blackOlivesCheckbox.AutoSize = True
+        Me.blackOlivesCheckbox.Location = New System.Drawing.Point(7, 102)
+        Me.blackOlivesCheckbox.Name = "blackOlivesCheckbox"
+        Me.blackOlivesCheckbox.Size = New System.Drawing.Size(85, 17)
+        Me.blackOlivesCheckbox.TabIndex = 2
+        Me.blackOlivesCheckbox.Text = "Black Olives"
+        Me.blackOlivesCheckbox.UseVisualStyleBackColor = True
+        '
+        'mushroomsCheckbox
+        '
+        Me.mushroomsCheckbox.AutoSize = True
+        Me.mushroomsCheckbox.Location = New System.Drawing.Point(7, 61)
+        Me.mushroomsCheckbox.Name = "mushroomsCheckbox"
+        Me.mushroomsCheckbox.Size = New System.Drawing.Size(80, 17)
+        Me.mushroomsCheckbox.TabIndex = 1
+        Me.mushroomsCheckbox.Text = "Mushrooms"
+        Me.mushroomsCheckbox.UseVisualStyleBackColor = True
+        '
+        'extraCheeseCheckbox
+        '
+        Me.extraCheeseCheckbox.AutoSize = True
+        Me.extraCheeseCheckbox.Location = New System.Drawing.Point(7, 20)
+        Me.extraCheeseCheckbox.Name = "extraCheeseCheckbox"
+        Me.extraCheeseCheckbox.Size = New System.Drawing.Size(89, 17)
+        Me.extraCheeseCheckbox.TabIndex = 0
+        Me.extraCheeseCheckbox.Text = "Extra Cheese"
+        Me.extraCheeseCheckbox.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Controls.Add(Me.TextBox2)
+        Me.GroupBox3.Controls.Add(Me.totalTextbox)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox3.Location = New System.Drawing.Point(23, 230)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(482, 121)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Transanction Details"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(281, 89)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Change"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(245, 32)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Amount Tendered"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Total Price"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Quantity"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(354, 86)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(354, 29)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'totalTextbox
+        '
+        Me.totalTextbox.Location = New System.Drawing.Point(69, 86)
+        Me.totalTextbox.Name = "totalTextbox"
+        Me.totalTextbox.Size = New System.Drawing.Size(100, 20)
+        Me.totalTextbox.TabIndex = 1
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(69, 30)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 0
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.ListBox1)
+        Me.GroupBox4.Location = New System.Drawing.Point(527, 40)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(284, 409)
+        Me.GroupBox4.TabIndex = 3
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Todays' Sales Summary"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(10, 22)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(263, 342)
+        Me.ListBox1.TabIndex = 0
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.btnExit)
+        Me.GroupBox5.Controls.Add(Me.btnClear)
+        Me.GroupBox5.Controls.Add(Me.btnCalculate)
+        Me.GroupBox5.Location = New System.Drawing.Point(23, 375)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(482, 65)
+        Me.GroupBox5.TabIndex = 4
+        Me.GroupBox5.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(340, 30)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(177, 30)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(18, 30)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.BackgroundImage = Global.Romans_Pizza_vb.My.Resources.Resources.RP_Logo_TM
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(827, 508)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "Form1"
+        Me.Text = "Romans Pizza"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox5.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents largeCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents mediumCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents smallCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents tomatoesCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents greenPepperCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents onionsCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents blackOlivesCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents mushroomsCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents extraCheeseCheckbox As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents totalTextbox As System.Windows.Forms.TextBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents GroupBox5 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+
+End Class
---
+++ Romans Pizza vb/Romans Pizza vb/Form1.vb
@@ -0,0 +1,112 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalculate.Click
+        Dim small As Double = 25.75
+        Dim medium As Double = 69.46
+        Dim large As Double = 98.21
+        Dim extraCheese As Double = 5.12
+        Dim mushrooms As Double = 5.75
+        Dim blackOlives As Double = 5.25
+        Dim onions As Double = 4.0
+        Dim greenPepper As Double = 4.5
+        Dim tomatoes As Double = 4.25
+        Dim change As Double
+        Dim total As Double
+        Dim amountGiven As Double
+        Dim pizzaSize As Double
+        Dim pizzaToppings As Double
+
+       
+
+        If (smallCheckbox.Checked) = True Then
+            pizzaSize = pizzaSize + small
+        End If
+        If mediumCheckbox.Checked = True Then
+            pizzaSize = pizzaSize + medium
+        End If
+        If largeCheckbox.Checked = True Then
+            pizzaSize = pizzaSize + large
+        End If
+        If extraCheeseCheckbox.Checked = True Then
+
+            pizzaToppings = pizzaToppings + extraCheese
+        End If
+        If mushroomsCheckbox.Checked = True Then
+            pizzaToppings = pizzaToppings + mushrooms
+        End If
+        If blackOlivesCheckbox.Checked = True Then
+            pizzaToppings = pizzaToppings + blackOlives
+        End If
+        If onionsCheckbox.Checked = True Then
+            pizzaToppings = pizzaToppings + onions
+        End If
+        If greenPepperCheckbox.Checked = True Then
+            pizzaToppings = pizzaToppings + greenPepper
+        End If
+        If tomatoesCheckbox.Checked = True Then
+            pizzaToppings = pizzaToppings + tomatoes
+
+        End If
+
+
+        total = (pizzaSize + pizzaToppings) * NumericUpDown1.Value
+        totalTextbox.Text = total
+
+        amountGiven = TextBox2.Text
+        change = amountGiven - total
+        TextBox3.Text = change
+
+        ListBox1.Items.Add("==========================================")
+
+        If smallCheckbox.Checked Then
+            ListBox1.Items.Add("Pizza size : Small")
+        End If
+        If mediumCheckbox.Checked Then
+            ListBox1.Items.Add("Pizza size : Medium")
+        End If
+        If largeCheckbox.Checked Then
+            ListBox1.Items.Add("Pizza size : Large")
+        End If
+
+
+        ListBox1.Items.Add("Quantity : " & NumericUpDown1.Value)
+        ListBox1.Items.Add("Total Cost : " & total.ToString)
+        ListBox1.Items.Add("Amount Tendered : " & amountGiven)
+        ListBox1.Items.Add("Change : " & change)
+        ListBox1.Items.Add("==========================================")
+        ListBox1.Items.Add("                                          ")
+        ListBox1.Items.Add("==========================================")
+
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnClear.Click
+        smallCheckbox.Checked = False
+        mediumCheckbox.Checked = False
+        largeCheckbox.Checked = False
+        extraCheeseCheckbox.Checked = False
+        mushroomsCheckbox.Checked = False
+        blackOlivesCheckbox.Checked = False
+        onionsCheckbox.Checked = False
+        greenPepperCheckbox.Checked = False
+        tomatoesCheckbox.Checked = False
+        totalTextbox.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+
+
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnExit.Click
+        MsgBox("Please make a selection if you wish to exit")
+
+    End Sub
+
+
+    Private Sub extraCheeseCheckbox_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles extraCheeseCheckbox.CheckedChanged
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
